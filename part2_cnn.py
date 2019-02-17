c1c5bc4f7d5498c71b4b915493119f837627bab1
Francis de Ladurantaye <fdl9044@gmail.com>
2019-02-17 18:48:21 -0500

    merged new part 2

--- part2_cnn.py
+++ part2_cnn.py
@@ -2,8 +2,7 @@
 """
 Created on Sun Feb 10 21:45:43 2019
 
-Structure Adapted from:
-https://github.com/pytorch/examples/blob/master/mnist/main.py#L39
+Adapted from "https://github.com/pytorch/examples/blob/master/mnist/main.py#L39"
 """
 
 
@@ -14,16 +13,48 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import  numpy as np
+import gzip, pickle
 import matplotlib.pyplot as plt
 from torchsummary import summary
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, TensorDataset
 from torchvision import transforms, utils
 import torchvision.datasets as datasets
-
+import time
 #Ignore warnings
 import warnings 
 warnings.filterwarnings("ignore")
 
+
+#%%
+#Build custom dataset loafin
+class MNIST_dataset(Dataset):
+    def __init__(self, data, target, transform=None):
+        self.data = torch.from_numpy(data).float()
+        self.target = torch.from_numpy(target).long()
+        self.transform = transform
+        
+    def __getitem__(self, index):
+        x = self.data[index]
+        y = self.target[index]
+        
+        if self.transform:
+            x = self.transform(x)
+        
+        return x, y
+    
+    def __len__(self):
+        return len(self.data)
+    
+def create_loader(data, labels, batch_size):
+    dataset = MNIST_dataset(np.expand_dims(data, axis=1), labels)
+    loader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=True
+    )
+    return loader
+
+#%%
 #Define the structure of Neural Network
 class MNIST_CNN(nn.Module):
     
@@ -64,7 +95,7 @@ class MNIST_CNN(nn.Module):
 #Define Training Method
 def train(args, model, device, train_loader, optimizer, epoch):
         model.train()
-        
+        train_loss = 0
         #define the operation batch-wise
         for batch_idx, (data, target) in enumerate(train_loader):
             #send the data into GPU or CPU
@@ -74,17 +105,24 @@ def train(args, model, device, train_loader, optimizer, epoch):
             #get out
             output = model(data)
             #define loss
-            loss = F.cross_entropy(output, target)
+            loss = F.nll_loss(output, target)
             #do backprobagation to get gradient
             loss.backward()
             #update the parameters
             optimizer.step()
+            
+            #train loss
+            train_loss+=loss.item()
+            
             #show the training progress
-            if batch_idx % args.log_interval == 0:
+            if batch_idx % 500 == 0:
                 #loss.item() gets the a scalar value held in the loss
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
+    
+    
+        return train_loss/len(train_loader)
     
 #Define Test Method
 def test(args, model, device, test_loader):
@@ -97,7 +135,7 @@ def test(args, model, device, test_loader):
                 data, target = data.to(device), target.to(device)
                 output = model(data)
                 #sum up batch loss
-                test_loss += F.cross_entropy(output, target, reduction='sum').item()
+                test_loss += F.nll_loss(output, target, reduction='sum').item()
                 #get the index of the max log-probability
                 pred = output.argmax(dim=1, keepdim=True)
                 #calculate the right classification
@@ -106,18 +144,14 @@ def test(args, model, device, test_loader):
         #Average the loss (batch_wise)
         test_loss /= len(test_loader.dataset)
         
-        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        print('\nValid set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
-def count_parameters(model):
-    n_params = 0
-    for param in model.parameters():
-        if param.requires_grad:
-            n_params += param.numel()
-    return n_params
+        return test_loss
 
-def main():    
+def main():
+    
     print("Pytorch Version:", torch.__version__)
     parser = argparse.ArgumentParser(description='TP1 MNIST CNN')
     #Training args
@@ -154,22 +188,28 @@ def main():
     #kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
     
     #MNIST dataset
-    mnist_train = datasets.MNIST(root='./data', train=True, download=True, transform=transforms.Compose([
-                           transforms.ToTensor()
-                       ]))
-    mnist_test = datasets.MNIST(root='./data', train=False, download=False, transform=transforms.Compose([
-                           transforms.ToTensor()
-                       ]))
     
-    train_loader = torch.utils.data.DataLoader(mnist_train, batch_size=args.batch_size, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(mnist_test, batch_size=args.test_batch_size, shuffle=True)
+    f = gzip.open('mnist.pkl.gz')
+    data = pickle.load(f, encoding='latin1')
+    
+    train_data, train_labels = data[0]
+    train_data = train_data.reshape(50000,28,28)
+    valid_data, valid_labels = data[1]
+    valid_data = valid_data.reshape(10000,28,28)
+    test_data, test_labels = data[2]
+    test_data = test_data.reshape(10000,28,28)
+    
+    #create dataloader
+    train_loader = create_loader(train_data, train_labels, args.batch_size)
+    valid_loader = create_loader(valid_data, valid_labels, args.batch_size)
+    test_loader = create_loader(test_data, test_labels, args.batch_size)
+
     
     #create Neural Network Object
     model = MNIST_CNN().to(device)
+    
     #print the model summery
     print(model)
-    #print parameter count
-    print('\nParameter count:', count_parameters(model), '\n')
     
     #Visualize the output of each layer via torchSummary
     summary(model, (1, 28, 28))
@@ -177,10 +217,38 @@ def main():
     #choose the optimizer 
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
     
+    
+    train_loss = []
+    validation_loss = []
+    
+    start_time = time.time()
     #Start Training
     for epoch in range(1, args.epochs+1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(args, model, device, test_loader)
+        #train loss
+        t_loss = train(args, model, device, train_loader, optimizer, epoch)
+        #validation loss
+        v_loss = test(args, model, device, valid_loader)
+        
+        train_loss.append(t_loss)
+        validation_loss.append(v_loss)
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+    #plot train/validation loss versus epoch
+    x = list(range(1, 11))
+    plt.figure()
+    plt.title("train/validation loss versus epoch")
+    plt.xlabel("epoch")
+    plt.ylabel("total loss")
+    plt.plot(x, train_loss,label="train loss")
+    plt.plot(x, validation_loss, color='red', label="validation loss")
+    plt.legend(loc='upper right')
+    plt.grid(True)
+    plt.show()
+    
+    
+    #test set
+    print ("Test Accuracy:")
+    v_loss = test(args, model, device, test_loader)
     
     #Save the trained model(which means parameters)
     if(args.save_model):
@@ -188,11 +256,3 @@ def main():
         
 if __name__ == '__main__':
     main()
-    
-
-
-
-
-
-
-
